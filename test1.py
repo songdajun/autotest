e4e8f862f53186528ae18d0bb44b65e28fe2bae9
songdajun <1657154715@qq.com>
2019-11-27 08:32:10 +0800

    第二次

---
+++ test1.py
@@ -0,0 +1,41 @@
+123 = str(input("a:"))
+b = int(input("b:"))
+c = float(input("c:"))
+print(type(a))
+print(type(b))
+print(type(c))
+d = "{} {} {}".format(a,b,c)
+print(d)
+
+a = int(input("a:"))
+b = int(input("b:"))
+print(a+b)
+
+b = input("b:")
+print(bool(type(b)==str))
+
+a = input("a:")
+print(a)
+print(bool(a=="1"))
+
+a = input("a:")
+print(type(a))
+print(bool(a == "小明"))
+
+#print(type(None))
+
+#range(2)
+qq = ["1","2","3","4"]
+for i in range(len(qq)):
+    print(qq[i])
+
+a = int(input("a:"))
+print(type(a))
+print(bool(isinstance(a,int)))
+
+
+
+
+
+
+
